e00bc3dc97992c7635a5030c40b18c7c8e7dfad5
hobbyguy77 <markmak77@gmail.com>
2016-07-05 11:44:46 +0300

    Video V1

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -2,16 +2,35 @@
 var init = function () {
 	// TODO:: Do your initialization job
 	console.log("init() called");
+	
+	$("#str1").css("color", "red").css("font-size", "40");
+	
+	$("#str2").css("color", "orange").css("text-decoration", "underline");
+	
+	$("#str3").css("color", "yellow").css("font-weight", "bold");
+	
+	$("#str4").css("color", "green").css("font-style", "italic");
+	
+	$("#str5").css("color", "blue").css("text-decoration", "line-through");
+	
+	$("#str6").css("color", "violet");
 
 	// add eventListener for tizenhwkey
 	document.addEventListener('tizenhwkey', function(e) {
-		if(e.keyName == "back") {
-			try {
-				tizen.application.getCurrentApplication().exit();
-			} catch (error) {
-				console.error("getCurrentApplication(): " + error.message);
-			}
-		}
+		if( e.keyName === "back" ) {
+            var activePopup = document.querySelector( '.ui-popup-active' ),
+                page = document.getElementsByClassName( 'ui-page-active' )[0],
+                pageid = page ? page.id : "";
+
+            if( pageid === "one" && !activePopup ) {
+                try {
+                    tizen.application.getCurrentApplication().exit();
+                } catch (ignore) {
+                }
+            } else {
+                window.history.back();
+            }
+        }
 	});
 };
 // window.onload can work without <body onload="">
